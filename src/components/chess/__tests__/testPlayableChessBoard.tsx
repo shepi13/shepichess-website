df92597773a8da70f0fcbf40166eed811195a47a
shepi13 <shepi13@gmail.com>
2025-04-05 12:15:19 -0400

    Playable chess board test stubs

---
+++ src/components/chess/__tests__/testPlayableChessBoard.tsx
@@ -0,0 +1,15 @@
+import { act } from "@testing-library/react";
+import { root } from "@/components/__tests__/componentTestHelpers"
+import { startFen } from "@/lib/types/pgnTypes";
+import { PlayableChessBoard } from "../PlayableChessBoard";
+
+
+describe("Test PlayableChessBoard", () => {
+
+    beforeEach(() => {
+        act(() => root.render(<PlayableChessBoard start={startFen} />));
+    });
+
+    test("Test Playable Chessboard", () => {});
+    test("Test Custom FlipText", () => {});
+});
