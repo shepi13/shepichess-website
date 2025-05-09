40befae112ace38fdf1b9d4cb5f60fc30a6b65ce
shepi13 <shepi13@gmail.com>
2025-05-09 01:54:54 -0400

    Made PGNViewerScrollable, cleaned up useVariation hook

--- src/app/articles/openings/layout.tsx
+++ src/app/articles/openings/layout.tsx
@@ -4,11 +4,11 @@ export default function OpeningLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="pb-20 flex flex-row-reverse gap-5">
-      <div className="flex flex-col min-w-1/4 items-end">
+    <div className="pb-20 flex flex-row-reverse gap-5 justify-center">
+      <div className="hidden md:flex flex-col min-w-1/4 items-end">
         <h5>Table Of Contents!</h5>
       </div>
-      <div className="flex flex-col">{children}</div>
+      <div className="flex flex-col max-w-3/4">{children}</div>
     </div>
   );
 }
